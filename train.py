49c88fafbfb4b98172ae67f36ded875031d5981d
Igor Zolotarev <zolotarev.igi@gmail.com>
2024-05-06 16:00:51 +0200

    Fix XGBoost

--- train.py
+++ train.py
@@ -5,6 +5,7 @@ import warnings
 import xgboost as xgb
 import numpy as np
 from test import TestNeuralNetwork
+from sklearn.metrics import accuracy_score
 
 class TrainNeuralNetwork():
     def __init__(self, config):
@@ -27,11 +28,12 @@ class TrainNeuralNetwork():
             self.cnnTrainLoop(model, loss, optimizer, TrainDataLoader, ValidationDataLoader)
         elif self.config['model_name'] == 'XGBoost':
             model = xgb.XGBClassifier()
-            self.xgbTrainLoop(model, TrainDataLoader)
+            self.xgbTrainLoop(model, TrainDataLoader, ValidationDataLoader)
         else:
             raise ValueError("Please choose either VGG or XGBoost")
 
-        if self.config['save_model']:
+        if self.config['save_model']: #ne ulazi ovde
+            print("usaooo")
             torch.save(model, SAVED_MODEL_PATH + self.config['model_name'] + '_model.pth')
 
     def cnnTrainLoop(self, model, loss, optimizer, TrainDataLoader, ValidationDataLoader):
@@ -63,14 +65,31 @@ class TrainNeuralNetwork():
                 prev_f1_score = f1_score
 
 
-    def xgbTrainLoop(self, model, DataLoader):
-        features, labels = [], []
-        for i, (images, labels) in enumerate(DataLoader):
+    def xgbTrainLoop(self, model, DataLoader, ValidationDataLoader):
+        features, all_labels = [], []
+        for i, (images, batch_labels) in enumerate(DataLoader):
             print(f'Batch: {i}, Index: {i*BATCH_SIZE}')
             images = images.to(DEVICE)
             features.append(images)
-            labels.append(labels.numpy())
+            all_labels.append(batch_labels.numpy().tolist())
         
         features = np.concatenate(features, axis=0)
-        labels = np.concatenate(labels, axis=0)
-        model.fit(features, labels)
+        features_2d = features.reshape(features.shape[0], -1)
+        labels = np.concatenate(all_labels, axis=0)
+        
+        model.fit(features_2d, labels)
+
+        # Use the ValidationDataLoader for validation predictions
+        validation_features, validation_labels = [], []
+        for images, batch_labels in ValidationDataLoader:
+            images = images.to(DEVICE)
+            validation_features.append(images)
+            validation_labels.append(batch_labels.numpy().tolist())
+
+        validation_features = np.concatenate(validation_features, axis=0)
+        validation_features_2d = validation_features.reshape(validation_features.shape[0], -1)
+        validation_labels = np.concatenate(validation_labels, axis=0)
+        validation_predictions = model.predict(validation_features_2d)
+
+        validation_accuracy = accuracy_score(validation_labels, validation_predictions)
+        print(f'Validation Accuracy: {validation_accuracy}')
